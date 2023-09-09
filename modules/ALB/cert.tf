ebb06c887b5cfdc9b77b96967dc01e2f8ccc1a8b
Tosin <inlukobet@gmail.com>
2023-09-09 23:03:28 +0100

    cert

--- modules/ALB/cert.tf
+++ modules/ALB/cert.tf
@@ -1,13 +1,6 @@
 # The entire section create a certificate, public zone, and validate the certificate using DNS method
 
 # Create the certificate using a wildcard for all the domains created in awsbukola.fun
-resource "aws_acm_certificate" "" {
-  domain_name       = "*awsbukola.fun"
-  validation_method = "DNS"
-}
-# The entire section create a certificate, public zone, and validate the certificate using DNS method
-
-# Create the certificate using a wildcard for all the domains created in awsbukola.fun
 resource "aws_acm_certificate" "awsbukola" {
   domain_name       = "*awsbukola.fun"
   validation_method = "DNS"
@@ -23,62 +16,6 @@ data "aws_route53_zone" "awsbukola" {
 resource "aws_route53_record" "awsbukola" {
   for_each = {
     for dvo in aws_acm_certificate.awsbukola.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
-  }
-
-  allow_overwrite = true
-  name            = each.value.name
-  records         = [each.value.record]
-  ttl             = 60
-  type            = each.value.type
-  zone_id         = data.aws_route53_zone.awsbukola.zone_id
-}
-
-# validate the certificate through DNS method
-resource "aws_acm_certificate_validation" "awsbukola" {
-  certificate_arn         = aws_acm_certificate.awsbukola.arn
-  validation_record_fqdns = [for record in aws_route53_record.awsbukola : record.fqdn]
-}
-
-# create records for tooling
-resource "aws_route53_record" "tooling" {
-  zone_id = data.aws_route53_zone.awsbukola.zone_id
-  name    = "tooling.awsbukola.fun"
-  type    = "A"
-
-  alias {
-    name                   = aws_lb.ext-alb.dns_name
-    zone_id                = aws_lb.ext-alb.zone_id
-    evaluate_target_health = true
-  }
-}
-
-
-# create records for wordpress
-resource "aws_route53_record" "wordpress" {
-  zone_id = data.aws_route53_zone.awsbukola.zone_id
-  name    = "wordpress.awsbukola.fun"
-  type    = "A"
-
-  alias {
-    name                   = aws_lb.ext-alb.dns_name
-    zone_id                = aws_lb.ext-alb.zone_id
-    evaluate_target_health = true
-  }
-}
-# calling the hosted zone
-data "aws_route53_zone" "awsbukola" {
-  name         = "awsbukola.fun"
-  private_zone = false
-}
-
-# selecting validation method
-resource "aws_route53_record" "awsbukola" {
-  for_each = {
-    for dvo in aws_acm_certificate.project_19_cert.domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
       record = dvo.resource_record_value
       type   = dvo.resource_record_type
